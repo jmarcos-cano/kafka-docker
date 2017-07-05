3ca5386b3bbc2dc07936aa74cb91d558b21c55fc
Marcos Cano <>
2017-07-04 21:19:15 -0600

    Replaces wait command and adds better --v

--- docker/include/scripts/create-topics.sh
+++ docker/include/scripts/create-topics.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 if [[ -z "$START_TIMEOUT" ]]; then
     START_TIMEOUT=600
 fi
@@ -8,8 +7,16 @@ fi
 start_timeout_exceeded=false
 count=0
 step=10
-while netstat -lnt | awk '$4 ~ /:'$KAFKA_PORT'$/ {exit 1}'; do
-    echo "waiting for kafka to be ready"
+
+KAFKA_PORT=$( echo $KAFKA_ADVERTISED_LISTENERS | grep -oE "[[:digit:]]{1,}" |head -n 1 )
+
+WAIT_COMMAND="netstat -lnt | awk '$4 ~ /:'${KAFKA_PORT}'$/ {exit 1}'"
+#run jmx on random port 9
+WAIT_COMMAND="JMX_PORT=9 $KAFKA_HOME/bin/zookeeper-shell.sh ${KAFKA_ZOOKEEPER_CONNECT} <<< 'ls /brokers/ids' | awk 'NR > 1 {print $1}' RS='[' FS=']'"
+
+
+while [[ -z $(eval $WAIT_COMMAND) ]]; do
+    echo "[CREATE-TOPICS] - waiting for kafka to be ready"
     sleep $step;
     count=$(expr $count + $step)
     if [ $count -gt $START_TIMEOUT ]; then
@@ -19,18 +26,20 @@ while netstat -lnt | awk '$4 ~ /:'$KAFKA_PORT'$/ {exit 1}'; do
 done
 
 if $start_timeout_exceeded; then
-    echo "Not able to auto-create topic (waited for $START_TIMEOUT sec)"
+    echo "[CREATE-TOPICS] - Not able to auto-create topic (waited for $START_TIMEOUT sec)"
     exit 1
 fi
 
 if [[ -n $KAFKA_CREATE_TOPICS ]]; then
     IFS=','; for topicToCreate in $KAFKA_CREATE_TOPICS; do
-        echo "creating topics: $topicToCreate"
+        echo "[CREATE-TOPICS] - ********* CREATING TOPICS: ********* \n [$topicToCreate]"
         IFS=':' read -a topicConfig <<< "$topicToCreate"
         if [ ${topicConfig[3]} ]; then
-          JMX_PORT='' $KAFKA_HOME/bin/kafka-topics.sh --create --zookeeper $KAFKA_ZOOKEEPER_CONNECT --replication-factor ${topicConfig[2]} --partition ${topicConfig[1]} --topic "${topicConfig[0]}" --config cleanup.policy="${topicConfig[3]}"
+          JMX_PORT='' $KAFKA_HOME/bin/kafka-topics.sh --create --zookeeper $KAFKA_ZOOKEEPER_CONNECT --replication-factor ${topicConfig[2]} --partition ${topicConfig[1]} --topic "${topicConfig[0]}" --config cleanup.policy="${topicConfig[3]}" | xargs echo "[CREATE-TOPICS] - "
         else
-          JMX_PORT='' $KAFKA_HOME/bin/kafka-topics.sh --create --zookeeper $KAFKA_ZOOKEEPER_CONNECT --replication-factor ${topicConfig[2]} --partition ${topicConfig[1]} --topic "${topicConfig[0]}"
+          JMX_PORT='' $KAFKA_HOME/bin/kafka-topics.sh --create --zookeeper $KAFKA_ZOOKEEPER_CONNECT --replication-factor ${topicConfig[2]} --partition ${topicConfig[1]} --topic "${topicConfig[0]}" | xargs echo "[CREATE-TOPICS] - "
         fi
     done
+else 
+    echo "[CREATE-TOPICS] - ********* NO TOPICS PROVIDED *********"
 fi
